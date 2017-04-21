34bd6b3cb81420a691fb760e33fc2527d2e3a12d
Connie Dang <conniedang576@gmail.com>
2017-04-20 22:23:11 -0400

    working on modal

--- JS/scripts.js
+++ JS/scripts.js
@@ -33,11 +33,20 @@ $(document).ready(function(){
 				// console.log(youtubeLink)
 				var nowPlayingHTML = '';
 				nowPlayingHTML += '<div class="col-sm-3 eachMovie">';
-					nowPlayingHTML += '<a href="'+youtubeLink+'"><img src="'+poster+'"></a>';
-					nowPlayingHTML += '<div class="release">Release date: '+releaseDate+'</div>';
-					nowPlayingHTML += '<div class="overview">' +overview+ '</div>';// Put overview in a separate div to make it easier to style
-					nowPlayingHTML += '<div class="rating">Rating: '+voteAverage+ '/10</div>';
+					nowPlayingHTML += '<button type="button" class="btn eachMovie" data-toggle="modal" data-target=".bs-example-modal-lg">'+'<img src="'+poster+'"></button>'; 	
+					nowPlayingHTML += '<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">';
+						nowPlayingHTML += '<div class="modal-dialog modal-sm" role="document">';
+							nowPlayingHTML += '<div class="modal-content">';
+							nowPlayingHTML += '</div>' //close modal-content
+						nowPlayingHTML += '</div>'; //close modal-dialog
+					nowPlayingHTML += '</div>'; //close modal
+
+					// nowPlayingHTML += '<a href="'+youtubeLink+'"><img src="'+poster+'"></a>';
+					// nowPlayingHTML += '<div class="release">Release date: '+releaseDate+'</div>';
+					// nowPlayingHTML += '<div class="overview">' +overview+ '</div>';// Put overview in a separate div to make it easier to style
+					// nowPlayingHTML += '<div class="rating">Rating: '+voteAverage+ '/10</div>';
 				nowPlayingHTML += '</div>'; //close off div 
+
 				$('#movie-grid').append(nowPlayingHTML);//Without this line, there is nowhere for the posters and overviews to display so it doesn't show up 
 
 			})
@@ -45,3 +54,25 @@ $(document).ready(function(){
 
 	}) 
 });
+
+
+
+
+// nowPlayingHTML += '<div class="col-sm-3 eachMovie">';
+// 	nowPlayingHTML += '<div class="modal fade" tabindex="-1" role="dialog">';
+// 		nowPlayingHTML += ' <div class="modal-dialog" role="document">';
+// 			nowPlayingHTML += '<div class="modal-content">';
+// 				nowPlayingHTML += '<div class="modal-header">';
+
+// 					nowPlayingHTML += '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+// 					nowPlayingHTML += '<h4 class="modal-title">Modal title</h4>';
+// 				nowPlayingHTML += '</div>'; //close off modal-header
+// 				nowPlayingHTML += '<div class="modal-body">';
+// 					nowPlayingHTML += '<div class="overview>' +overview+ '</div>';
+// 				nowPlayingHTML += '</div>'; //close off modal-body
+// 				nowPlayingHTML += '<div class="modal-footer">';
+// 				nowPlayingHTML += '</div>'; //close off modal-footer
+// 			nowPlayingHTML += '</div>';
+// 		nowPlayingHTML += '</div>';
+// 	nowPlayingHTML += '</div>';
+// nowPlayingHTML += '</div>';
