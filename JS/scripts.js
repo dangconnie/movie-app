d9bfd44e70acaad0ca06b8583e0f27f84bbeb26f
Connie Dang <conniedang576@gmail.com>
2017-04-22 15:31:42 -0400

    modal

--- JS/scripts.js
+++ JS/scripts.js
@@ -32,20 +32,16 @@ $(document).ready(function(){
 				var youtubeLink = 'https://www.youtube.com/watch?v='+youtubeKey;
 				// console.log(youtubeLink)
 				var nowPlayingHTML = '';
-			
-			// tvShowHTML += '<div class="eachTvShow"><button type="button" class="btn invisible-btn" data-toggle="modal" data-target="#exampleModalTV' + i + '" data-whatever="@' + i + '"><img src="' + imageBaseUrl + 'w300' + tvData.results[i].poster_path + '"></button>'
-
-			// tvShowHTML += '<div class="modal fade" id="exampleModalTV' + i + '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"><div class="modal-dialog" role="document"><div class="modal-content">'
-
-
 				nowPlayingHTML += '<div class="col-sm-3 eachMovie">';
-					nowPlayingHTML += '<button type="button" class="btn eachMovie" data-toggle="modal" data-target=".popupModal">'+'<img src="'+poster+'"></button>'; 	
-					nowPlayingHTML += '<div class="modal fade popupModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">';
-						nowPlayingHTML += '<div class="modal-dialog modal-lg" role="document">';
-							nowPlayingHTML += '<div class="modal-content">';
-								nowPlayingHTML += '<div class="overview">' +overview+ '</div>';
+					nowPlayingHTML += '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">'+'<img src="'+poster+'"></button>'; 	
+					nowPlayingHTML += '<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">';
+						nowPlayingHTML += '<div class="modal-dialog" role="document">';
+							nowPlayingHTML += '<div class="modal-content col-sm-12">';
+								nowPlayingHTML += '<a href="'+youtubeLink+'"><img src="'+poster+'"></a>'; 
+								nowPlayingHTML += '<div class="movieName">'+mid+'</div>';
 								nowPlayingHTML += '<div class="release">Release date: '+releaseDate+'</div>';
-								nowPlayingHTML += '<div class="rating">Rating: '+voteAverage+ '/10</div>'
+								nowPlayingHTML += '<div class="overview">' +overview+ '</div>';// Put overview in a separate div to make it easier to style
+								nowPlayingHTML += '<div class="rating">Rating: '+voteAverage+ '/10</div>';
 							nowPlayingHTML += '</div>' //close modal-content
 						nowPlayingHTML += '</div>'; //close modal-dialog
 					nowPlayingHTML += '</div>'; //close modal
