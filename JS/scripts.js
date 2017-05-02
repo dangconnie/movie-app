8d8bac4fc38f1f29912ddb1cc7beb171ea1ffcd0
Connie Dang <conniedang576@gmail.com>
2017-05-01 23:37:24 -0400

    html built

--- JS/scripts.js
+++ JS/scripts.js
@@ -165,7 +165,6 @@ $(document).ready(function(){
 							genreHTML += '</div>'; //close modal-dialog
 						genreHTML += '</div>'; //close modal
 					genreHTML += '</div>'; //close off each div
-
 					$('#movie-grid').append(genreHTML);
 					//Without this line, there is nowhere for the posters and overviews to display so it doesn't show up 
 					// $('#movieGenreLabel').html("Now Playing");
@@ -284,51 +283,54 @@ $(document).ready(function(){
 		const searchMovieURL = apiBaseURL + 'search/movie?api_key=' + apiKey + '&language=en-US&page=1&include_adult=false&query=' + searchTerm;
 			// console.log(searchMovieURL);
 		$.getJSON(searchMovieURL, function(movieSearchResults){
-			console.log(movieSearchResults);
-			// for (var i = 0; i<movieSearchResults.length; i++){
-			// 	var poster = imageBaseUrl+'w300'+movieSearchResults.results[i].poster_path;
-			// 	var title = movieSearchResults.results[i].original_title;
-			// 	var releaseDate = movieSearchResults.results[i].release_date;
-			// 	var overview = movieSearchResults.results[i].overview;
-			// 	var voteAverage = movieSearchResults.results[i].vote_average;				
-			// 	var youtubeKey = movieKey.results[0].key;
-			// 	var youtubeLink = 'https://www.youtube.com/watch?v='+youtubeKey;
-			// 	var searchResultsHTML = '';
-			// 	searchResultsHTML += '<div class="col-sm-3 col-md-3 col-lg-3 eachMovie">';
-			// 		searchResultsHTML += '<button type="button" class="btnModal" data-toggle="modal" data-target="#exampleModal'+ i + '" data-whatever="@' + i + '">'+'<img src="'+poster+'"></button>'; 	
-			// 		searchResultsHTML += '<div class="modal fade" id="exampleModal' + i +'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">';
-			// 			searchResultsHTML += '<div class="modal-dialog" role="document">';
-			// 				searchResultsHTML += '<div class="modal-content col-sm-12 col-lg-12">';
-			// 					searchResultsHTML += '<div class="col-sm-6 moviePosterInModal">';
-			// 						searchResultsHTML += '<a href="'+youtubeLink+'"><img src="'+poster+'"></a>'; 
-			// 					searchResultsHTML += '</div><br>';//close trailerLink
-			// 					searchResultsHTML += '<div class="col-sm-6 movieDetails">';
-			// 						searchResultsHTML += '<div class="movieName">'+title+'</div><br>';
-			// 						searchResultsHTML += '<div class="linkToTrailer"><a href="'+youtubeLink+'"><span class="glyphicon glyphicon-play"></span>&nbspPlay trailer</a>' + '</div><br>';	
-			// 						searchResultsHTML += '<div class="release">Release Date: '+releaseDate+'</div><br>';
-			// 						searchResultsHTML += '<div class="overview">' +overview+ '</div><br>';
-			// 						searchResultsHTML += '<div class="rating">Rating: '+voteAverage+ '/10</div><br>';
-			// 						searchResultsHTML += '<div class="col-sm-3 btn btn-primary">8:30 AM' + '</div>';
-			// 						searchResultsHTML += '<div class="col-sm-3 btn btn-primary">10:00 AM' + '</div>';
-			// 						searchResultsHTML += '<div class="col-sm-3 btn btn-primary">12:30 PM' + '</div>';
-			// 						searchResultsHTML += '<div class="col-sm-3 btn btn-primary">3:00 PM' + '</div>';
-			// 						searchResultsHTML += '<div class="col-sm-3 btn btn-primary">4:10 PM' + '</div>';
-			// 						searchResultsHTML += '<div class="col-sm-3 btn btn-primary">5:30 PM' + '</div>';
-			// 						searchResultsHTML += '<div class="col-sm-3 btn btn-primary">8:00 PM' + '</div>';
-			// 						searchResultsHTML += '<div class="col-sm-3 btn btn-primary">10:30 PM' + '</div>';
-			// 					searchResultsHTML += '</div>'; //close movieDetails
-			// 			searchResultsHTML += '</div>'; //close modal-dialog
-			// 		searchResultsHTML += '</div>'; //close modal
-			// 	searchResultsHTML += '</div>'; //close off each div
+			// console.log(movieSearchResults);
+			for (let i = 0; i<movieSearchResults.results.length; i++){
+				var mid = movieSearchResults.results[i].id;
+				var thisMovieUrl = apiBaseURL+'movie/'+mid+'/videos?api_key=' + apiKey;		
 
-			// 	$('#searchResults').append(searchResultsHTML);
-			// 	//Without this line, there is nowhere for the posters and overviews to display so it doesn't show up 
-			// 	// $('#movieGenreLabel').html("Now Playing");
-			// 	//h1 will change depending on what is clicked. Will display "Now Playing" in this case.
-			// }
+				$.getJSON(thisMovieUrl, function(movieKey){
+					var poster = imageBaseUrl+'w300'+movieSearchResults.results[i].poster_path;
+					var title = movieSearchResults.results[i].original_title;
+					var releaseDate = movieSearchResults.results[i].release_date;
+					var overview = movieSearchResults.results[i].overview;
+					var voteAverage = movieSearchResults.results[i].vote_average;				
+					var youtubeKey = movieKey.results[0].key;
+					var youtubeLink = 'https://www.youtube.com/watch?v='+youtubeKey;
+					var searchResultsHTML = '';
+					searchResultsHTML += '<div class="col-sm-3 col-md-3 col-lg-3 eachMovie">';
+						searchResultsHTML += '<button type="button" class="btnModal" data-toggle="modal" data-target="#exampleModal'+ i + '" data-whatever="@' + i + '">'+'<img src="'+poster+'"></button>'; 	
+						searchResultsHTML += '<div class="modal fade" id="exampleModal' + i +'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">';
+							searchResultsHTML += '<div class="modal-dialog" role="document">';
+								searchResultsHTML += '<div class="modal-content col-sm-12 col-lg-12">';
+									searchResultsHTML += '<div class="col-sm-6 moviePosterInModal">';
+										searchResultsHTML += '<a href="'+youtubeLink+'"><img src="'+poster+'"></a>'; 
+									searchResultsHTML += '</div><br>';//close trailerLink
+									searchResultsHTML += '<div class="col-sm-6 movieDetails">';
+										searchResultsHTML += '<div class="movieName">'+title+'</div><br>';
+										searchResultsHTML += '<div class="linkToTrailer"><a href="'+youtubeLink+'"><span class="glyphicon glyphicon-play"></span>&nbspPlay trailer</a>' + '</div><br>';	
+										searchResultsHTML += '<div class="release">Release Date: '+releaseDate+'</div><br>';
+										searchResultsHTML += '<div class="overview">' +overview+ '</div><br>';
+										searchResultsHTML += '<div class="rating">Rating: '+voteAverage+ '/10</div><br>';
+										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">8:30 AM' + '</div>';
+										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">10:00 AM' + '</div>';
+										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">12:30 PM' + '</div>';
+										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">3:00 PM' + '</div>';
+										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">4:10 PM' + '</div>';
+										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">5:30 PM' + '</div>';
+										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">8:00 PM' + '</div>';
+										searchResultsHTML += '<div class="col-sm-3 btn btn-primary">10:30 PM' + '</div>';
+									searchResultsHTML += '</div>'; //close movieDetails
+							searchResultsHTML += '</div>'; //close modal-dialog
+						searchResultsHTML += '</div>'; //close modal
+					searchResultsHTML += '</div>'; //close off each div
+					console.log(searchResultsHTML)
+					$('#searchResults').append(searchResultsHTML);
+					//Label will be whatever user input was
+					$('#movieGenreLabel').html($('.form-control').val());	
+				})
+			}
 		})
 	}
-	// searchMovies();
 });
 
 
